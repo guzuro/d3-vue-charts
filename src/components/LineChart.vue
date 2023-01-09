64148147d0153b6973cd2f65fa7531d724ba6deb
anton.n <guzurogmn@gmail.com>
2023-01-09 19:05:57 +0300

    added date tooltip on marker

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -5,7 +5,9 @@
             ref="lineChart"
             :style="{
                 height: `${containerHeight}px`,
-            }"
+                            position: 'relative'
+
+                            }"
         >
             <div
                 class="line-chart__tooltip"
@@ -16,12 +18,12 @@
             />
             <button @click="zoomIn">+</button>
             <button @click="zoomOut">-</button>
+<!--          <div class="line-chart__marker-date" />-->
         </div>
-        <!-- <chart-tooltip
+         <chart-tooltip
             v-if="options.legend"
-            :infos="mappedLegend"
-            @infoClick="handle"
-        /> -->
+            :infos="selectedInfos"
+        />
     </div>
 </template>
 
@@ -33,15 +35,25 @@ import { scaleLinear, scaleTime } from "d3-scale";
 import { zoom } from "d3-zoom";
 import { pointer } from "d3-selection";
 import dayjs from "dayjs";
-import { Component, Mixins } from "vue-property-decorator";
+import { Component, Emit, Mixins } from 'vue-property-decorator'
 import D3Chart from "../d3Chart";
 import formatNumber from "../logic/formatNumber";
-import { timeParse } from "d3";
+import ChartTooltip from '@/components/ChartTooltip.vue'
+import { select } from 'd3'
 
-@Component
+@Component({
+    components: {
+        ChartTooltip
+    }
+})
 export default class extends Mixins<D3Chart>(D3Chart) {
-    get containerHeight(): number {
-        return this.options.chart.height;
+
+    @Emit('onMove')
+    selectedLabel(data:Array<any>):Array<any> {
+        return data.map(d => ({
+            name: d.name,
+            value: d.value,
+        }))
     }
 
     xScale = scaleTime();
@@ -59,202 +71,245 @@ export default class extends Mixins<D3Chart>(D3Chart) {
         .y((d) => this.yScale(d.value))
         .curve(curveMonotoneX);
 
-    setScales(): void {
-        const lt = this.data.labels.map((d) => new Date(d));
+  selectedInfos:any[] = []
 
-        this.xScale.domain(d3Extent(lt) as any).range([0, this.size.width]);
+  get bisect() {
+      return bisector((d: any) => new Date(d.label));
+  }
 
-        this.yScale
-            .range([0, this.size.height])
-            .domain([this.maxValueFromOptionsData, 0]);
+  get markerLine():any {
+      return this.svgGroup
+          .append("line")
+          .attr("class", "line-chart__marker")
+          .attr("x1", 0)
+          .attr("x2", 0)
+          .attr("y1", 0)
+          .attr("y2", this.size.height + this.margins.top)
+          .attr("stroke-width", 1)
+          .attr("stroke-dasharray", 7)
+          .attr("stroke", "black");
+  }
+
+  get markerDate() {
+    if (this.chartRoot) {
+      return this.chartRoot
+          .append('div')
+          .attr('class', 'line-chart__marker-date')
+        .style('opacity', 0)
     }
 
-    zoomIn() {
-        if (this.zoom) {
-            this.svg.call(this.zoom.scaleBy, 1.5);
-        }
-    }
+    return null
+  }
 
-    zoomed(e: any): void {
-        this.xScale.range(
-            [0, this.size.width - this.margins.right].map((d) =>
-                e.transform.applyX(d)
-            )
-        );
+  // get markerDate():any {
+  //   return
+  // }
 
-        this.svg.select(".chart-axis-group-x").call(this.setAxis);
-        this.svgGroup.selectAll(".path").attr("d", (d) => this.line(d.value));
+  get zoom() {
+      if (this.options.chart.zoom) {
+          return zoom()
+              .scaleExtent([1, 8])
+              .translateExtent(this.extent)
+              .extent(this.extent)
+              .on("zoom", this.zoomed);
+      }
 
-        this.svgGroup
-            .selectAll("circle")
-            .attr("cx", (d) => this.xScale(new Date(d.label)))
-            .attr("cy", (d) => this.yScale(d.value));
-    }
+      return null;
+  }
 
-    zoomOut() {
-        if (this.zoom) {
-            this.svg.call(this.zoom.scaleBy, 0.5);
-        }
-    }
+  get containerHeight(): number {
+      return this.options.chart.height;
+  }
 
-    setChartAxis(): void {
-        const axisGroup = this.svgGroup
-            .append("g")
-            .attr("class", "chart-axis-group");
+  setScales(): void {
+      const lt = this.data.labels.map((d) => new Date(d));
 
-        this.yAxis = axisGroup.append("g").attr("class", "chart-axis-group--y");
+      this.xScale.domain(d3Extent(lt) as any).range([0, this.size.width]);
 
-        this.xAxis = axisGroup.append("g").attr("class", "chart-axis-group-x");
-    }
+      this.yScale
+          .range([0, this.size.height])
+          .domain([this.maxValueFromOptionsData, 0]);
+  }
 
-    get zoom() {
-        if (this.options.chart.zoom) {
-            return zoom()
-                .scaleExtent([1, 8])
-                .translateExtent(this.extent)
-                .extent(this.extent)
-                .on("zoom", this.zoomed);
-        }
+  zoomIn() {
+      if (this.zoom) {
+          this.svg.call(this.zoom.scaleBy, 1.5);
+      }
+  }
 
-        return null;
-    }
+  zoomed(e: any): void {
+      this.xScale.range(
+          [0, this.size.width - this.margins.right].map((d) =>
+              e.transform.applyX(d)
+          )
+      );
 
-    setAxis(): void {
-        const xAxisOption = this.options.xAxis;
-        const yAxisOption = this.options.yAxis;
-        const lt = this.data.labels
-            .map((d) => new Date(d))
-            .filter((d, i, arr) => (arr.length > 15 ? i % 2 === 0 : true));
+      this.svg.select(".chart-axis-group-x").call(this.setAxis);
+      this.svgGroup.selectAll(".path").attr("d", (d) => this.line(d.value));
 
-        if (yAxisOption && yAxisOption.visible) {
-            this.yAxis.call(
-                axisLeft(this.yScale).tickFormat((t, i) => formatNumber(t))
-            );
-        }
+      this.svgGroup
+          .selectAll("circle")
+          .attr("cx", (d) => this.xScale(new Date(d.label)))
+          .attr("cy", (d) => this.yScale(d.value));
+  }
 
-        if (xAxisOption && xAxisOption.visible) {
-            this.xAxis
-                .attr("transform", `translate(0,${this.size.height})`)
-                .call(
-                    axisBottom(this.xScale)
-                        .tickValues(lt)
-                        .tickFormat(this.formatTick)
-                );
-        }
-    }
+  zoomOut() {
+      if (this.zoom) {
+          this.svg.call(this.zoom.scaleBy, 0.5);
+      }
+  }
 
-    formatTick(date: any): string {
-        const format = this.options.xAxis.format;
+  setChartAxis(): void {
+      const axisGroup = this.svgGroup
+          .append("g")
+          .attr("class", "chart-axis-group");
 
-        if (format) {
-            return dayjs(date).format(format);
-        }
+      this.yAxis = axisGroup.append("g").attr("class", "chart-axis-group--y");
 
-        return dayjs(date).format("D MMM");
-    }
+      this.xAxis = axisGroup.append("g").attr("class", "chart-axis-group-x");
+  }
 
-    setLines() {
-        const groupes = Array.from(
-            group(this.chartData, (d) => d.name),
-            ([key, value]) => ({ key, value })
-        );
 
-        const lines = this.svgGroup
-            .selectAll("lines")
-            .data(groupes)
-            .enter()
-            .append("g");
+  setAxis(): void {
+      const xAxisOption = this.options.xAxis;
+      const yAxisOption = this.options.yAxis;
+      const lt = this.data.labels
+          .map((d) => new Date(d))
+          .filter((d, i, arr) => (arr.length > 15 ? i % 2 === 0 : true));
 
-        lines
-            .append("path")
-            .attr("d", (d) => this.line(d.value))
-            .attr("stroke", (d) => d.value[0].color)
-            .attr("stroke-width", 2)
-            .attr("class", "path");
+      if (yAxisOption && yAxisOption.visible) {
+          this.yAxis.call(
+              axisLeft(this.yScale).tickFormat((t, i) => formatNumber(t))
+          );
+      }
 
-        this.setDots();
-    }
+      if (xAxisOption && xAxisOption.visible) {
+          this.xAxis
+              .attr("transform", `translate(0,${this.size.height})`)
+              .call(
+                  axisBottom(this.xScale)
+                      .tickValues(lt)
+                      .tickFormat(this.formatTick)
+              );
+      }
+  }
 
-    setDots(): void {
-        this.svgGroup
-            .selectAll("circle")
-            .append("g")
-            .data(this.chartData)
-            .enter()
-            .append("circle")
-            .attr("class", "chart_circle")
-            .attr("r", (d) => (d.value !== null ? 3 : 0))
-            .attr("cx", (d) => this.xScale(new Date(d.label)))
-            .attr("cy", (d) => this.yScale(d.value))
-            .attr("fill", (d) => d.color);
-    }
+  formatTick(date: any): string {
+      const format = this.options.xAxis.format;
 
-    onResize(): void {
-        this.setSizes();
-        this.setSvgViewBox();
-        this.setScales();
-        this.setAxis();
+      if (format) {
+          return dayjs(date).format(format);
+      }
 
-        this.svgGroup.selectAll(".path").attr("d", (d) => this.line(d.value));
+      return dayjs(date).format("D MMM");
+  }
 
-        this.svgGroup
-            .selectAll("circle")
-            .attr("r", (d) => (d.value !== null ? 2 : 0))
-            .attr("cx", (d) => this.xScale(new Date(d.label)))
-            .attr("cy", (d) => this.yScale(d.value));
-    }
+  setLines() {
+      const groupes = Array.from(
+          group(this.chartData, (d) => d.name),
+          ([key, value]) => ({ key, value })
+      );
+
+      const lines = this.svgGroup
+          .selectAll("lines")
+          .data(groupes)
+          .enter()
+          .append("g");
+
+      lines
+          .append("path")
+          .attr("d", (d) => this.line(d.value))
+          .attr("stroke", (d) => d.value[0].color)
+          .attr("stroke-width", 2)
+          .attr("class", "path");
+
+      this.setDots();
+  }
+
+  setDots(): void {
+      this.svgGroup
+          .selectAll("circle")
+          .append("g")
+          .data(this.chartData)
+          .enter()
+          .append("circle")
+          .attr("class", "chart_circle")
+          .attr("r", (d) => (d.value !== null ? 3 : 0))
+          .attr("cx", (d) => this.xScale(new Date(d.label)))
+          .attr("cy", (d) => this.yScale(d.value))
+          .attr("fill", (d) => d.color);
+  }
+
+  onResize(): void {
+      this.setSizes();
+      this.setSvgViewBox();
+      this.setScales();
+      this.setAxis();
+
+      this.svgGroup.selectAll(".path").attr("d", (d) => this.line(d.value));
+
+      this.svgGroup
+          .selectAll("circle")
+          .attr("r", (d) => (d.value !== null ? 2 : 0))
+          .attr("cx", (d) => this.xScale(new Date(d.label)))
+          .attr("cy", (d) => this.yScale(d.value));
+  }
+
+  onMousemove(e: MouseEvent):void {
+      const label = this.xScale.invert(e.pageX);
+
+      const nearestIndex = this.bisect.center(this.chartData, label);
+
+      const nearestIndexData = this.chartData[nearestIndex];
+      const x = this.xScale(new Date(nearestIndexData.label));
+
+      this.selectedInfos = this.chartData.filter(cd => cd.label === nearestIndexData.label);
+
+      this.markerDate!.style('opacity', 1)
+          .style('left', `${x + this.margins.left - (this.markerDate!.node()!.clientWidth / 2)}px`)
+          .style('margin-top', `-${this.margins.bottom}px`)
+          .html(this.formatTick(nearestIndexData.label))
+
+
+      this.markerLine.attr("x1", x).attr("x2", x).attr("opacity", 1);
+  }
+
+  onMouseleave(e: MouseEvent):void {
+    const n = select('.line-chart__marker-date');
+
+        n.style('opacity', 0)
+
+  }
 
     mounted(): void {
-        this.initData(this.$refs.lineChart as Element, "line");
-        this.setSizes();
+      this.initData(this.$refs.lineChart as Element, "line");
+      this.setSizes();
 
-        this.setSvgViewBox();
-        this.setChartAxis();
+      this.setSvgViewBox();
+      this.setChartAxis();
 
-        this.setScales();
-        this.setAxis();
+      this.setScales();
+      this.setAxis();
 
-        this.setLines();
+      this.setLines();
+      // this.markerDate
 
-        this.extent = [
-            [0, 0],
-            [
-                this.size.width - this.margins.right,
-                this.size.height - this.margins.top,
-            ],
-        ];
+      this.extent = [
+          [0, 0],
+          [
+              this.size.width - this.margins.right,
+              this.size.height - this.margins.top,
+          ],
+      ];
 
-        this.svg.call(this.zoom).on("wheel.zoom", null);
+      this.svg.call(this.zoom).on("wheel.zoom", null);
 
-        const markerLine = this.svgGroup
-            .append("line")
-            .attr("class", "line-chart__marker")
-            .attr("x1", 0)
-            .attr("x2", 0)
-            .attr("y1", 0)
-            .attr("y2", this.size.height + this.margins.top)
-            .attr("stroke-width", 2)
-            .attr("stroke-dasharray", 7)
-            .attr("stroke", "black");
+      this.svg
+          .on("mousemove", this.onMousemove)
+    .on('mouseleave', this.onMouseleave)
 
-        const bisect = bisector((d: any) => new Date(d.label));
-
-        this.svg.on("mousemove", (e) => {
-            const pointerCoords = pointer(e);
-            const [posX, posY] = pointerCoords;
-
-            const label = this.xScale.invert(posX - 25);
-
-            const index = bisect.center(this.chartData, label);
-
-            const d = this.chartData[index];
-            const x = this.xScale(new Date(d.label));
-
-            markerLine.attr("x1", x).attr("x2", x).attr("opacity", 1);
-        });
-
-        window.addEventListener("resize", this.onResize);
-    }
+      window.addEventListener("resize", this.onResize);
+  }
 }
 </script>
 
@@ -263,6 +318,13 @@ export default class extends Mixins<D3Chart>(D3Chart) {
     &__marker {
         opacity: 0.2;
     }
+
+  &__marker-date {
+    border: 1px black solid;
+    position: absolute;
+    background-color: grey;
+    font-size: 11.5px;
+  }
 }
 path {
     fill: none;
